89b8d2cd11bc18e5700fa6f1cfe1ee0851509cd3
iishu <ishikasrivastava27594@gmail.com>
2024-08-09 23:25:49 +0530

    add components

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="flex h-[98vh] gap-4">
+        <div className=" w-1/5 border border-gray-500 rounded p-2">hello</div>
+        <div className=" w-5/6">hello2</div>
+    </div>
+  )
+}
+
+export default Home
