b8188a76d0ad768e00c412c51c330740343eb4b2
iishu <ishikasrivastava27594@gmail.com>
2024-08-13 19:50:50 +0530

    input form

--- frontend/src/pages/AllTasks.jsx
+++ frontend/src/pages/AllTasks.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Cards from '../components/Home/Cards'
 import { IoIosAdd } from "react-icons/io";
-import InputData from "../components/Home/InputData";
+import InputData from "../components/Home/InputData"; 
 const AllTasks = () => {
+  const [InputDiv , setInputDiv] = useState("hidden")
   
   return (
    <>
     <div> 
       <div className="w-full flex justify-end px-4 py-2">
-        <button> 
+        <button onClick={()=>setInputDiv("fixed")}> 
         <IoIosAdd className='text-4xl text-gray-300 hover:text-gray-100 transition-all duration-300'/>
         </button>
         </div>
       <Cards home={"true"}/>
     </div>
-    <InputData />
+    <InputData InputDiv={InputDiv} setInputDiv={setInputDiv}/>
     </>
   )
 }
